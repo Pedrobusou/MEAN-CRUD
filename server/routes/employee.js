67219b80f803036ea15dc7d66bcfc33a2aef7c78
Pedro <pedrobusou@gmail.com>
2019-10-29 20:40:09 +0100

    Create /api/employees route

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json({
+        status: 'Api works'
+    });
 });
 
 module.exports = router;
