9d7a6386e3e944e2e35b48751d9a0fbf46e217d7
Pedro <pedrobusou@gmail.com>
2019-10-30 23:13:08 +0100

    Add mongoose options
    to prevent using deprecated stuff and having warnings messages

--- server/database.js
+++ server/database.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/mean-crud';
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect(URI)
     .then(db => console.log('DB conected'))
     .catch(e => console.error(error));
