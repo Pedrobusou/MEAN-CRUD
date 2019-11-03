2433ff0aecf6db98d527614dcc322fd4db9ceaea
Pedro <pedrobusou@gmail.com>
2019-11-03 10:24:31 +0100

    Import employee model and HttpClient

--- client/src/app/services/employee.service.ts
+++ client/src/app/services/employee.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  selectedEmployee: Employee;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
