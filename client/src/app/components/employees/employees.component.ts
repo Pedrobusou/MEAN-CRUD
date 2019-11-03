4809d8676961c6a70c038e8fd85102e7c44d2adf
Pedro <pedrobusou@gmail.com>
2019-11-03 11:29:33 +0100

    Add reset form function

--- client/src/app/components/employees/employees.component.ts
+++ client/src/app/components/employees/employees.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service'
+import { NgForm } from '@angular/forms';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employees',
@@ -12,6 +14,13 @@ export class EmployeesComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+
   }
 
+}
+
+  resetForm(form: NgForm) {
+    form.reset();
+    this.employeeService.selectedEmployee = new Employee();
+  }
 }
