837c7a0f079d17b39b5096d76173481e4be8ba3b
Pedro <pedrobusou@gmail.com>
2019-11-03 11:13:18 +0100

    Import Employee service on Employee component

--- client/src/app/components/employees/employees.component.ts
+++ client/src/app/components/employees/employees.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service'
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.sass']
+  styleUrls: ['./employees.component.sass'],
+  providers: [EmployeeService]
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
