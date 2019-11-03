82fcd4368bf16c74c71d16448265cbb743a47cdc
Pedro <pedrobusou@gmail.com>
2019-11-03 11:06:02 +0100

    Add interaction with api on  Employee service

--- client/src/app/services/employee.service.ts
+++ client/src/app/services/employee.service.ts
@@ -6,7 +6,27 @@ import { Employee } from '../models/employee';
   providedIn: 'root'
 })
 export class EmployeeService {
+  readonly API_URL = 'http://localhost:3000/api/employees';
   selectedEmployee: Employee;
+  items: Employee[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.selectedEmployee = new Employee();
+  }
+
+  create(item: Employee) {
+    this.http.post(this.API_URL, item);
+  }
+
+  read() {
+    return this.http.get(this.API_URL);
+  }
+
+  update(item: Employee) {
+    this.http.put(`${this.API_URL}/${item._id}`, item);
+  }
+
+  delete(_id: string) {
+    return this.http.delete(`${this.API_URL}/${_id}`);
+  }
 }
